bd0fe69d130808d47a4559b4397c1f48b92c1c24
GuYith <gxyss0@126.com>
2025-01-13 13:23:38 +0800

    BlogPost hover improve

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -8,7 +8,7 @@ const BlogPost = ({ post }) => {
 
   return (
     <Link href={`${BLOG.path}/${post.slug}`}>
-      <article key={post.id} className="mb-6 md:mb-8">
+      <article key={post.id} className="mb-6 md:mb-8 hover:-ml-8">
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
           <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
             {isUrl(post.pageIcon) ?
