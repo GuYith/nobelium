8442720fcdbebd2604e73f029c469ea45e5f54a6
GuYith <gxyss0@126.com>
2025-01-13 15:38:09 +0800

    add cover

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -8,12 +8,34 @@ const BlogPost = ({ post }) => {
 
   return (
     <Link href={`${BLOG.path}/${post.slug}`}>
-      <article key={post.id} className="mb-6 md:mb-8 hover:-ml-8">
+      {post.pageCover ?
+      <article key={post.id} className="mb-6 md:mb-8 group hover:-ml-8 hover:mr-8 flex justify-between relative">
+        <div >
+          <header className="flex flex-col justify-between md:flex-row md:items-baseline">
+            <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
+              {isUrl(post.pageIcon) ?
+            <img src={post.pageIcon} className="inline w-7 h-7"></img>
+          : post.pageIcon} {post.title}
+            </h2>
+          </header>
+          <main >
+            <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
+              {post.summary}
+            </p>
+          </main>
+          <time className="flex-shrink-0 text-gray-600 dark:text-gray-400 absolute -bottom-2">
+              <FormattedDate date={post.date} />
+          </time>
+        </div>
+          <img src={post.pageCover} className="ml-3 w-60 h-36 object-cover"></img>
+      </article>
+      :
+      <article key={post.id} className="mb-6 md:mb-8 hover:-ml-8 hover:mr-8">
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
           <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
             {isUrl(post.pageIcon) ?
-      <img src={post.pageIcon} style={{display: "inline", width:'28px', height:'100%'}}></img>
-     : post.pageIcon} {post.title}
+            <img src={post.pageIcon} className="inline w-7 h-7"></img>
+          : post.pageIcon} {post.title}
           </h2>
           <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
             <FormattedDate date={post.date} />
@@ -24,7 +46,8 @@ const BlogPost = ({ post }) => {
             {post.summary}
           </p>
         </main>
-      </article>
+    </article>
+    }
     </Link>
   );
 };
